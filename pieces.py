7a9d304878268eddc9df24c43011b835e0df2a02
Tyler Begrow <tjbegrow@gmail.com>
2022-08-01 08:54:48 -0500

    feat:Created pieces.py to create classes for the chess pieces

---
+++ pieces.py
@@ -0,0 +1,12 @@
+class ChessPiece:
+    def __init__(self, name, position):
+        self.name = name
+        self.position = position
+
+
+
+class Pawn(ChessPiece):
+    pass
+
+class Knight(ChessPiece):
+    pass
