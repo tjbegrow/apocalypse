f5f3d9058410d9e9d4012069fdee56d64753d544
Tyler Begrow <tjbegrow@gmail.com>
2022-08-02 22:23:40 -0500

    Feat: Symbols added to chess pieces

--- apocalypse.py
+++ apocalypse.py
@@ -8,23 +8,26 @@ ind = ['5', '4', '3', '2', '1']
 # Piece placement. BK = Black Kight BP = Black Pawn WK = White Kight 
 # WP = White Pawn
 
-initial_setup = [[pieces.Knight('BK1','a5'), 
-                  pieces.Pawn('BP2','b5'),
-                  pieces.Pawn('BP3','c5'),
-                  pieces.Pawn('BP4','d5'),
-                  pieces.Knight('BK2','e5')],
-                 [pieces.Pawn('BP1','a4'), 'O', 'O', 'O', 
-                  pieces.Pawn('BP5','e4'),],
+initial_setup = [[pieces.Knight('BK1', 'a5', '\u265E'), 
+                  pieces.Pawn('BP2', 'b5', '\u265F'),
+                  pieces.Pawn('BP3', 'c5', '\u265F'),
+                  pieces.Pawn('BP4', 'd5', '\u265F'),
+                  pieces.Knight('BK2', 'e5','\u265E')],
+                 [pieces.Pawn('BP1', 'a4', '\u265F'), 'O', 'O', 'O', 
+                  pieces.Pawn('BP5', 'e4', '\u265F'),],
                  ['O', 'O', 'O', 'O' ,'O'],
-                 [pieces.Knight('WP1','a2'), 'O', 'O', 'O',
-                  pieces.Knight('WP5','e2')],
-                 [pieces.Knight('WK1','a1'),
-                  pieces.Pawn('WP2','b1'),
-                  pieces.Pawn('WP3','c1'), 
-                  pieces.Pawn('WP4','d1'),
-                  pieces.Knight('WK2','e1')]]
+                 [pieces.Pawn('WP1', 'a2', '\u2659'), 'O', 'O', 'O',
+                  pieces.Pawn('WP5', 'e2', '\u2659')],
+                 [pieces.Knight('WK1', 'a1', '\u2658'),
+                  pieces.Pawn('WP2', 'b1', '\u2659'),
+                  pieces.Pawn('WP3', 'c1', '\u2659'), 
+                  pieces.Pawn('WP4', 'd1', '\u2659'),
+                  pieces.Knight('WK2', 'e1', '\u2658')]]
 
 chess_board = pd.DataFrame(initial_setup, ind, columns)
+
+chess_board_visual = [chess_board.at['5', 'a'].symbol]
+
 # print(chess_board)
 
 # BP1 = pieces.Pawn("BP1", "a4")
@@ -54,8 +57,6 @@ def select_piece():
                 print("You selected {} at position {}".format(piece_selected.name, piece_selected.position))
                 break
     return piece_selected
-            
-
 
 
 def select_coord():
@@ -94,3 +95,4 @@ The classic C.S. Lewis chess varient, by Tyler Begrow
 -----------------------------------------------------
 ''')
     current_piece = select_piece()
+    print(chess_board_visual)
