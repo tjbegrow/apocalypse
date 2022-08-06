d510215cc172ce603768d4bd6720a1cc7ebcc38b
Tyler Begrow <tjbegrow@gmail.com>
2022-08-05 20:57:22 -0500

    feat: Created a player class so player objects can hold game info

---
+++ players.py
@@ -0,0 +1,5 @@
+class Player:
+    def __init__(self, player_number, selected_piece, penalty)
+        self.player_number = player_number
+        self.selected_piece = selected_piece
+        self.penalty = penalty
